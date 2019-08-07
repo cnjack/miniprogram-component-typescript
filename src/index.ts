8b4f37882f8cf5d0699e5eaa2119508c74119dcb
jack <h_7357@qq.com>
2019-08-07 11:31:52 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/// <reference path="./types/index.d.ts" />
+
+Component({
+  properties: {
+    prop: {
+      type: String,
+      value: 'index.properties'
+    },
+  },
+  data: {},
+  lifetimes: {},
+  methods: {}
+})
